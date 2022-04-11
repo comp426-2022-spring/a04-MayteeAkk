c1dfab1c145612c50257757d1cdc27b3319414fb
MayteeAkk <mayteeoakk@gmail.com>
2022-04-11 01:05:33 -0400

    Added some more stuff

--- server.js
+++ server.js
@@ -22,26 +22,28 @@ const log = args.log || true;
 const fs = require('fs');
 const morgan = require('morgan');
 
-const help = (`
-server.js [options]
+//Starting an App Server
+const server = app.listen(port, () => {
+    console.log("App listening on port %PORT%".replace("%PORT%", port));
+});
 
---port	Set the port number for the server to listen on. Must be an integer
-            between 1 and 65535.
 
---debug	If set to true, creates endlpoints /app/log/access/ which returns
-            a JSON access log from the database and /app/error which throws 
-            an error with the message "Error test successful." Defaults to 
-            false.
-
---log		If set to false, no log files are written. Defaults to true.
-            Logs are always written to database.
-
---help	Return this message and exit.
-`)
-// If --help or -h, echo help text to STDOUT and exit
-if (args.help || args.h) {
-    console.log(help)
-    process.exit(0)
+if (args['help']) {
+    console.log(`server.js [options]
+    --port	Set the port number for the server to listen on. Must be an integer
+                between 1 and 65535.
+  
+    --debug	If set to 'true', creates endlpoints /app/log/access/ which returns
+                a JSON access log from the database and /app/error which throws 
+                an error with the message "Error test successful." Defaults to 
+                'false'.
+  
+    --log		If set to false, no log files are written. Defaults to true.
+                Logs are always written to database.
+  
+    --help	Return this message and exit.
+    `);
+    process.exit(0);
 }
 
 app.use((req, res, next) => {
@@ -96,43 +98,44 @@ if (log) {
     app.use(morgan('combined',{stream:writestream}))
 }
 
-//Starting an App Server
-const server = app.listen(port, () => {
-    console.log("App listening on port %PORT%".replace("%PORT%", port));
+app.get('/app/', (req, res) => {
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, {'Content-Type' : 'text/plain'});
+    res.end(res.statusCode+ ' ' +res.statusMessage)
 });
 
-app.get('/app', (req, res) => {
-    res.status(200).end("OK");
-    res.type("text/plain");
-})
-
-app.get('/app/echo/:number', (req, res) => {
-    res.status(200).json({ "message": req.params.number });
-})
-
-//Defining Check Endpoint
-app.get('/app/flip', (req, res) => {
-    let flip = coinFlip();
-    res.status(200).json({ "flip": flip })
-})
+app.get('/app/flip/', (req, res) => {
+    res.status(200);
+    const ans = coinFlip();
+    const flipResult = {"flip" : ans};
+    res.json(flipResult);
+});
 
 app.get('/app/flips/:number', (req, res) => {
-    const result = coinFlips(parseInt(req.params.number))
-    const count = countFlips(result)
-    res.status(200).json({"raw": result, "summary": count})
-})
-
-app.get('/app/flip/call/:call', (req, res) => {
-    res.status(200).json(flipACoin(req.params.call))
-})
-
-//Default Reponse for Any Other Request
-app.use(function(req, res) {
-    res.status(404).end("Endpoint does not exist");
-    res.type("text/plain");
+    res.status(200);
+    const flips = req.params.number || 1;
+    const values = coinFlips(flips);
+    const rawjson = {
+        "raw" : values,
+        "summary": countFlips(values)
+    };
+    res.json(rawjson)
 });
 
+app.get('/app/flip/call/heads', (req, res) => {
+    res.status(200);
+    res.json(flipACoin('heads'));
+});
 
+app.get('/app/flip/call/tails', (req, res) => {
+    res.status(200);
+    res.json(flipACoin('tails'));
+});
+
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
 
 
 
